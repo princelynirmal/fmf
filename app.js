368487a95981c1c25fac97adb1c22a3bab2f0b6f
Nirmal Princely <nirmal.princely@optus.com.au>
2016-07-25 13:15:32 +1000

    Changes to support OPENSHIFT

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var destination_address_name;
 // all environments
 // app.set('port', process.env.PORT || 8080);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('server_ip_address', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -96,7 +97,7 @@ app.get('/uberlogin', function(req, res) {
     res.redirect(authUrl);
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), server_ip_address, function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
